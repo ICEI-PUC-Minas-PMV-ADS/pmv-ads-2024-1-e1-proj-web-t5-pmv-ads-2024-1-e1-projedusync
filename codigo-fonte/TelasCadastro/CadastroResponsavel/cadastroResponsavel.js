5d845c5885fd3e9ebee272c35f16e1e6f5ed86c5
Juliane Guerra <165741792+julianeguerra@users.noreply.github.com>
2024-05-12 13:40:29 -0300

    Atualização JS

--- codigo-fonte/TelasCadastro/CadastroResponsavel/cadastroResponsavel.js
+++ codigo-fonte/TelasCadastro/CadastroResponsavel/cadastroResponsavel.js
@@ -3,10 +3,38 @@ function cadastrar(event) {
 
     var dadosCadastrais = {};
 
+    dadosCadastrais.nomesAlunosResp = [];
+
+    if (!!event.target.elements.nomeAlunoResp.value)
+        dadosCadastrais.nomesAlunosResp.push(event.target.elements.nomeAlunoResp.value);
+
     dadosCadastrais.nomeResp = event.target.elements.nomeResp.value;
     dadosCadastrais.emailResp = event.target.elements.emailResp.value
-    dadosCadastrais.nomeAlunoResp = event.target.elements.nomeAlunoResp.value
     dadosCadastrais.senhaResp = event.target.elements.senhaResp.value
 
+    var inputsAdicionais = document.querySelectorAll(".input-aluno-plus");
+
+    inputsAdicionais.forEach(x => {
+        if (!!x.value)
+            dadosCadastrais.nomesAlunosResp.push(x.value);
+    })
+
+    if (dadosCadastrais.nomesAlunosResp.length == 0)
+        alert("Adicione pelo menos um(a) aluno(a).");
+
     console.log(dadosCadastrais);
+}
+
+function adicionarAluno() {
+    var divAlunos = document.getElementById("inputs-alunos");
+
+    var input = document.createElement("input");
+
+    input.type = "text";
+    input.placeholder = "Nome do(a) aluno(a)";
+    input.name = "nomeAlunoRespPlus";
+    input.classList.add("input-aluno-plus");
+    
+
+    divAlunos.appendChild(input);
 }
