0d2773da5a6bc452572ce6661dfcd7dd329c333a
Juliane Guerra <165741792+julianeguerra@users.noreply.github.com>
2024-06-23 18:59:00 -0300

    Atualização JS + Página Avisos

--- codigo-fonte/TelasAvisos/AvisosResponsável/pagRegAvisosResp.js
+++ codigo-fonte/TelasAvisos/AvisosResponsável/pagRegAvisosResp.js
@@ -1,10 +1,31 @@
+preencherListaAlunos();
+
 function enviarAvisos() {
     var dadosAvisos = {};
  
     dadosAvisos.tituloAviso = document.getElementById('tituloAviso').value;  
     dadosAvisos.campoAviso = document.getElementById('campoAviso').value;
- 
+    dadosAvisos.aluno = document.getElementById('aluno').value;
+    dadosAvisos.perfil = 'resp';
+    dadosAvisos.idCriador = obterUsuarioLogado().id;
+    
     console.log(dadosAvisos);
+    adicionarAviso(dadosAvisos);
 }
+
+function preencherListaAlunos() {
+    let user = obterUsuarioLogado();
+    let select = document.getElementById('aluno');
+
+    user.nomesDeAluno.forEach(element => {
+        var opt = document.createElement('option');
+        console.log(element);
+        opt.value = element;
+        opt.innerHTML = element;
+        select.appendChild(opt);
+    });
+}
+
+
  
 
